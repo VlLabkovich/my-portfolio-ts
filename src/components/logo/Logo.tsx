53fa52aeb4636463e4e896b648a6b36dbee8d690
Vladislav <labkovich.vl@gmail.com>
2024-01-04 15:53:28 +0300

    feat: add component

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Icon} from "../icon/Icon";
+import styled from "styled-components";
+
+export const Logo = () => {
+    return (
+            <a href="">
+                <Icon iconId={'logo'} width={'50'} height={'50'} viewBox={'0 0 103 106'}/>
+            </a>
+    );
+};
