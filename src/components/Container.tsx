cbe2d32604633b23769b75edbe707f3e35495490
Vladislav <labkovich.vl@gmail.com>
2024-01-04 15:50:15 +0300

    feat: add container

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1170px;
+    width: 100%;
+    min-height: 100%;
+    padding: 0 15px;
+    margin: 0 auto;
+    border: 2px dashed red;
+`
