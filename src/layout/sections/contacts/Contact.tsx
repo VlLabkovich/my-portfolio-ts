3d783126f988bea264ed2ce55a33fb2b0e318896
Vladislav <labkovich.vl@gmail.com>
2024-01-12 18:45:58 +0300

    feat: add component

---
+++ src/layout/sections/contacts/Contact.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Container} from "../../../components/Container";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Button} from "../../../components/Button";
+
+export const Contacts: React.FC = () => {
+    return (
+        <StyledContacts>
+            <Container>
+                <SectionTitle>Contact Me</SectionTitle>
+                <StyledForm>
+                    {/*<FlexWrapper direction={'column'} justify={'center'} align={'flex-start'}>*/}
+                        <label>Name</label>
+                        <StyledInput placeholder={'Example'}/>
+                        <label>Email</label>
+                        <StyledInput placeholder={'Value'}/>
+                        <label>Message</label>
+                        <StyledText placeholder={'Hi, i will ...'}/>
+                        <StyledButton type={'submit'}>Submit</StyledButton>
+                </StyledForm>
+            </Container>
+        </StyledContacts>
+    );
+};
+
+const StyledContacts = styled.section`
+    min-height: 100vh;
+`
+
+const StyledForm = styled.form`
+    max-width: 532px;
+    width: 100%;
+    margin: 0 auto;
+`
+
+const StyledInput = styled.input`
+    max-width: 532px;
+    width: 100%;
+    height: 48px;
+`
+
+const StyledText = styled.textarea`
+    max-width: 532px;
+    width: 100%;
+    height: 165px;
+`
+
+const StyledButton = styled.button`
+    text-align: center;
+    margin-top: 22px;
+    max-width: 532px;
+    width: 100%;
+    height: 48px;
+    padding: 12px 20px;
+    border-radius: 6px;
+    background: #5222D0;
+    color: #DBFFFF;
+`
