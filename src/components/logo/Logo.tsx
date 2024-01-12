b6e7128c698853aee50c58e6137a4e6e2e5c5001
Vladislav <labkovich.vl@gmail.com>
2024-01-12 11:01:45 +0300

    refactor: change logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,8 +3,12 @@ import {Icon} from "../icon/Icon";
 
 export const Logo: React.FC = () => {
     return (
-            <a href="">
-                <Icon iconId={'logo'} width={'60'} height={'60'} viewBox={'0 0 103 106'}/>
-            </a>
+        <a href="">
+            <Icon iconId={'logo'} width={'70'} height={'70'} viewBox={'30 -30 80 150'}/>
+        </a>
     );
 };
+
+// <a href="">
+//     <Icon iconId={'logo'} width={'60'} height={'60'} viewBox={'0 0 103 106'}/>
+// </a>
