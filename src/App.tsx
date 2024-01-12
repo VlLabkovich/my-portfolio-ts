1f8ae2ec87cdbfe2007fc7999b565534631a54de
Vladislav <labkovich.vl@gmail.com>
2024-01-12 08:59:18 +0300

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
 import {AboutMe} from "./layout/sections/aboutMe/AboutMe";
 import {Skills} from "./layout/sections/skills/Skills";
+import {Projects} from "./layout/sections/projects/Projects";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Main/>
         <AboutMe/>
         <Skills/>
+        <Projects/>
     </div>
   );
 }
