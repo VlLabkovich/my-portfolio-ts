f0b2b3303dee5ce7d0d0fa2c874ce9f8ec0053a6
Vladislav <labkovich.vl@gmail.com>
2024-01-12 16:56:39 +0300

    refactor: change style

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -15,7 +15,7 @@ export const FlexWrapper = styled.div<FlexWrapperProps>`
     justify-content: ${props => props.justify || "flex-start"};
     align-items: ${props => props.align || "stretch"};
     flex-wrap: ${props => props.wrap || "no wrap"};
-    gap: ${props => props.gap || "10px"};
+    gap: ${props => props.gap};
     height: 100%;
 `
 
