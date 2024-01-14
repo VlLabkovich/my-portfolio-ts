7a8aaafe1aa34d38e5a2d3e2e980bcd4fa72695c
Vladislav <labkovich.vl@gmail.com>
2024-01-14 18:12:35 +0300

    feat: add component

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from "styled-components";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {Icon} from "../../components/icon/Icon";
+import {Menu} from "../../components/Menu";
+
+const itemsFooter = [
+    {
+        text: 'Projects',
+        href: 'https://yandex.by/search/?text=git+oy+sheet&lr=157'
+    }, {
+        text: 'Contact',
+        href: 'https://yandex.by/search/?text=git+oy+sheet&lr=157'
+    },
+];
+
+export const Footer: React.FC = () => {
+    return (
+        <StyledFooter>
+            <Container>
+                <FlexWrapper direction={'column'} align={'center'} justify={'center'}>
+                    <StyledFooterIcon>
+                        <a href="">
+                            <Icon iconId={'gmail'} width={'39'} height={'38'} viewBox={'0 0 39 38'}/>
+                            <StyledTitle>gmail</StyledTitle>
+                        </a>
+                        <a href="">
+                            <Icon iconId={'linkedin'} width={'39'} height={'38'} viewBox={'0 0 39 38'}/>
+                            <StyledTitle>linkedin</StyledTitle>
+                        </a>
+                        <a href="">
+                            <Icon iconId={'github'} width={'39'} height={'38'} viewBox={'0 0 39 38'}/>
+                            <StyledTitle>linkedin</StyledTitle>
+                        </a>
+                    </StyledFooterIcon>
+                    <Menu menuItems={itemsFooter}  StyledMenu={"secondary"}/>
+                    <Copyright>© 2024 Labkovich Vladislav, All Rights Reserved.</Copyright>
+                </FlexWrapper>
+            </Container>
+        </StyledFooter>
+    );
+};
+
+const StyledFooter = styled.footer`
+    min-height: 30vh;
+
+    a {
+        text-decoration: none;
+    }
+
+    span {
+        display: block;
+    }
+`
+
+const StyledFooterIcon = styled.div`
+    display: flex;
+    gap: 95px;
+`
+
+const StyledTitle = styled.span`
+`
+
+const StyledList = styled.ul`
+    gap: 48px;
+`
+
+const Copyright = styled.small`
+`
