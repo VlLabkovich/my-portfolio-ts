d22446b61d2591aaa55ef6f7e8be53e81fa28d71
Vladislav <labkovich.vl@gmail.com>
2024-01-17 22:27:25 +0300

    feat: add props and styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,55 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import {Theme} from "../styles/Theme";
 
-export const Button = styled.button`
 
+export type ButtonPropsType = {
+    btnType: string
+}
+
+export const Button = styled.button<ButtonPropsType>`
+    text-align: center;
+    font-size: inherit;
+    font-style: normal;
+    font-weight: inherit;
+    line-height: normal;
+    position: relative;
+    z-index: 0;
+
+
+    ${props => props.btnType === "primary" && css<ButtonPropsType>`
+        &::before {
+            content: "";
+            display: inline-block;
+            height: 10px;
+            width: 50%;
+            background-color: ${Theme.colors.accent};
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            transform: translateX(-50%);
+            z-index: -1;
+            //border-radius: 6px;
+        }
+
+        &:hover {
+            &:before {
+                display: inline-flex;
+                padding: 12px 20px;
+                height: 100%;
+                width: 100%;
+                border-radius: 6px;
+                justify-content: center;
+                align-items: center;
+                gap: 10px;
+            }
+
+            color: ${Theme.colors.link};
+        }
+    `
+    }
+
+    ${props => props.btnType === "secondary" && css<ButtonPropsType>`
+
+    `
+    }
 `
