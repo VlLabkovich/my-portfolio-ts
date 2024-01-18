20b94b9c76533a123352af59a271fd27ff30afac
Vladislav <labkovich.vl@gmail.com>
2024-01-18 03:07:17 +0300

    feat: add component

---
+++ src/layout/footer/FooterMenu.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "styled-components";
+import {Button} from "../../components/Button";
+
+export type MenuPropsType = {
+    menuItems: Array<{
+        text: string
+        href: string
+        btnType: string
+    }>
+}
+
+export const FooterMenu = (props: MenuPropsType) => {
+    return (
+        <StyledFooterMenu>
+            <ul>
+                {props.menuItems.map((item, index) => {
+                    return <li key={index}>
+                        <Button btnType={item.btnType} as={"a"} href={item.href}>{item.text}</Button>
+                    </li>
+                })}
+            </ul>
+        </StyledFooterMenu>
+    );
+};
+
+const StyledFooterMenu = styled.nav`
+    ul {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 48px;
+    }
+`
