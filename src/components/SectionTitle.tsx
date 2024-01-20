4e93201b409d0b9ab375d27bd8ddd98b0f6a2a96
Vladislav <labkovich.vl@gmail.com>
2024-01-20 12:31:52 +0300

    feat: add styles

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import {Theme} from "../styles/Theme";
 
 
 export const SectionTitle = styled.h2`
-
+    color: ${Theme.colors.font};
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 26px; /* 108.333% */
+    letter-spacing: 0.96px;
+    margin-bottom: 25px;
 `
