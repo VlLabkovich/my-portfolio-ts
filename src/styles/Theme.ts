34f5a638677eb091c4ab1f5a3e518744323e9a95
Vladislav <labkovich.vl@gmail.com>
2024-01-20 11:12:27 +0300

    feat: add styles

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,6 +3,7 @@ export const Theme = {
         accent: '#5222D0',
         font: '#151717',
         text: '#1F2626',
-        link: '#DBFFFF'
+        link: '#DBFFFF',
+        bgCard: '#F5F2FD'
     }
 }
