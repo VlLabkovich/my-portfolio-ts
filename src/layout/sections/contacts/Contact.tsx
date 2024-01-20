656d51a3005af5a2fa9af2542072a79d8fda8928
Vladislav <labkovich.vl@gmail.com>
2024-01-20 12:28:50 +0300

    feat: add styles

--- src/layout/sections/contacts/Contact.tsx
+++ src/layout/sections/contacts/Contact.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Container} from "../../../components/Container";
 import {Button} from "../../../components/Button";
+import {Theme} from "../../../styles/Theme";
 
 export const Contacts: React.FC = () => {
     return (
@@ -9,15 +10,13 @@ export const Contacts: React.FC = () => {
             <Container>
                 <SectionTitle>Contact Me</SectionTitle>
                 <StyledForm>
-                    <label>Name</label>
+                    <span>Name</span>
                     <StyledInput placeholder={'Example'}/>
-                    <label>Email</label>
+                    <span>Email</span>
                     <StyledInput placeholder={'Value'}/>
-                    <label>Message</label>
-                    <StyledText placeholder={'Hi, i will ...'}/>
-                    <StyledButton>
-                        <Button btnType={'secondary'} type={'submit'}>Submit</Button>
-                    </StyledButton>
+                    <span>Message</span>
+                    <StyledText placeholder={'Hi, i will ...'} as={'textarea'}/>
+                    <Button btnType={'secondary'} type={'submit'}>Submit</Button>
                 </StyledForm>
             </Container>
         </StyledContacts>
@@ -33,23 +32,53 @@ const StyledForm = styled.form`
     max-width: 532px;
     width: 100%;
     margin: 0 auto;
+
+    span {
+        display: block;
+        margin: 25px 0 6px;
+    }
+
+    textarea {
+        resize: none;
+        height: 165px;
+    }
+
+     input:first-of-type {
+        color: ${Theme.colors.first_input};
+        font-size: 14px;
+        font-weight: 400;
+        line-height: normal;
+    }
 `
 
 const StyledInput = styled.input`
+    border: 1px solid ${Theme.colors.accent};
     max-width: 532px;
     width: 100%;
     height: 48px;
+    padding: 15px 0 15px 18px;
+    border-radius: 6px;
+    border: 1px solid ${Theme.colors.border_input};
+
+    color: ${Theme.colors.text};
+    font-family: 'Montserrat',sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: normal;
 `
 
 const StyledText = styled.textarea`
     max-width: 532px;
     width: 100%;
     height: 165px;
+    padding: 15px 0 0 18px;
+    border-radius: 6px;
+    border: 2px solid ${Theme.colors.accent};
+
+    color: ${Theme.colors.text};
+    font-family: 'Montserrat',sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: normal;
 `
 
-const StyledButton = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-`
