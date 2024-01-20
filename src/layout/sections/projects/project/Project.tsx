efaf5f09082e118aca737846aad8eb9440ffda79
Vladislav <labkovich.vl@gmail.com>
2024-01-20 11:15:25 +0300

    refactor: change styles

--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
+import {Theme} from "../../../../styles/Theme";
 
 type ProjectPropsType = {
     image: string
@@ -32,36 +33,33 @@ export const Project: React.FC<ProjectPropsType> = (props: ProjectPropsType) => 
 
 const StyledProject = styled.div`
     max-width: 522px;
-    width: 100%;
     height: 636px;
-    box-shadow: 2px 2px 32px 0px rgba(40, 38, 44, 0.15);
+    box-shadow: 2px 2px 32px 0 rgba(40, 38, 44, 0.15);
     border-radius: 6px;
-
+    //background-color: ${Theme.colors.bgCard};
 `
 
 const StyledProjectImg = styled.img`
     width: 100%;
-    height: 388px;
+    height: 100%;
     object-fit: contain;
 `
 
-const Title = styled.h3`
-
-`
+const Title = styled.h3``
 
 const StyledContent = styled.div`
     //outline: 3px dashed grey;
-    margin-left: 26px;
+    margin: 29px 9px 32px 26px;
 `
 
 const StyledTags = styled.div`
     display: inline-flex;
     flex-wrap: wrap;
     gap: 12px;
+    padding: 11px 0 19px;
 `
 
 const Tags = styled.span`
-
     color: #DBFFFF;
     padding: 8px 16px;
     justify-content: center;
