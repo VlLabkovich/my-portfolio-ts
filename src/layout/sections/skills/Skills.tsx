048fc1a0542142bdc27a8743efc8411ea60317c2
Vladislav <labkovich.vl@gmail.com>
2024-01-20 18:27:01 +0300

    refactor: add icons

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -11,30 +11,34 @@ export const Skills: React.FC = () => {
         <StyledSkills id="skills">
             <Container>
                 <SectionTitle>Tech Skills</SectionTitle>
-                <FlexWrapper justify={'space-between'} align={'center'} wrap={'wrap'} gap={'120px'}>
-                        <Skill iconId={'html'}
-                               title={'HTML'}/>
-                        <Skill iconId={'css3'}
-                               title={'CSS3'}/>
-                        <Skill iconId={'js'}
-                               title={'JAVASCRIPT'}/>
-                        <Skill iconId={'ts'} width={'105'}
-                               title={'TYPESCRIPT'}/>
-                        <Skill iconId={'react'}
-                               title={'REACT JS'}/>
-                        <Skill iconId={'sc'}
-                               title={'STYLED COMPONENTS'}/>
-                        <Skill iconId={'bootstrap'}
-                               width={'88'}
-                               height={'120'}
-                               viewBox={'0 0 88 88'}
-                               title={'BOOTSTRAP'}/>
-                        <Skill iconId={'docker'} viewBox={'-10 10 130 100'}
-                               title={'DOCKER'}/>
-                        <Skill iconId={'git'} viewBox={'-6 -4 117 102'}
-                               title={'GIT'}/>
-                        <Skill iconId={'gitHub'} viewBox={'0 -10 86 95'}
-                               title={'GITHUB'}/>
+                <FlexWrapper justify={'space-between'} align={'center'} wrap={'wrap'} gap={'70px'}>
+                    <Skill iconId={'vscode'} viewBox={'0 0 112 112'}
+                           title={'VS-CODE'}/>
+                    <Skill iconId={'html'}
+                           title={'HTML'}/>
+                    <Skill iconId={'css3'}
+                           title={'CSS3'}/>
+                    <Skill iconId={'js'}
+                           title={'JAVASCRIPT'}/>
+                    <Skill iconId={'ts'} width={'105'}
+                           title={'TYPESCRIPT'}/>
+                    <Skill iconId={'react'}
+                           title={'REACT JS'}/>
+                    <Skill iconId={'sc'}
+                           title={'STYLED COMPONENTS'}/>
+                    <Skill iconId={'bootstrap'}
+                           width={'88'}
+                           height={'120'}
+                           viewBox={'0 0 88 88'}
+                           title={'BOOTSTRAP'}/>
+                    <Skill iconId={'docker'} viewBox={'-10 10 130 100'}
+                           title={'DOCKER'}/>
+                    <Skill iconId={'figma'} viewBox={'0 0 100 100'}
+                           title={'FIGMA'}/>
+                    <Skill iconId={'git'} viewBox={'-6 -4 117 102'}
+                           title={'GIT'}/>
+                    <Skill iconId={'gitHub'} viewBox={'0 -10 86 95'}
+                           title={'GITHUB'}/>
                 </FlexWrapper>
             </Container>
         </StyledSkills>
