cbb93ea3b6a2e7f904aeec325c0341a57dbedd2f
Vladislav <labkovich.vl@gmail.com>
2024-01-22 15:50:55 +0300

    refactor: add array objects

---
+++ src/layout/sections/skills/skill/SkillData.tsx
@@ -0,0 +1,71 @@
+
+export const skillData = [
+    {
+        iconId: 'vscode',
+        title: 'VS-CODE',
+        viewBox: '0 0 112 112'
+    },
+
+    {
+        iconId: 'html',
+        title: 'HTML'
+    },
+
+    {
+        iconId: 'css3',
+        title: 'CSS3'
+    },
+
+    {
+        iconId: 'js',
+        title: 'JAVASCRIPT'
+    },
+
+    {
+        iconId: 'ts',
+        title: 'TYPESCRIPT',
+        width: '105'
+    },
+
+    {
+        iconId: 'react',
+        title: 'REACT JS'
+    },
+
+    {
+        iconId: 'sc',
+        title: 'STYLED COMPONENTS'
+    },
+
+    {
+        iconId: 'bootstrap',
+        title: 'BOOTSTRAP',
+        width: '88',
+        height: '120',
+        viewBox: '0 0 88 88'
+    },
+
+    {
+        iconId: 'docker',
+        title: 'DOCKER',
+        viewBox: '-10 10 130 100'
+    },
+
+    {
+        iconId: 'figma',
+        title: 'FIGMA',
+        viewBox: '0 0 100 100'
+    },
+
+    {
+        iconId: 'git',
+        title: 'GIT',
+        viewBox: '-6 -4 117 102'
+    },
+
+    {
+        iconId: 'gitHub',
+        title: 'GITHUB',
+        viewBox: '0 -10 86 95'
+    },
+];
