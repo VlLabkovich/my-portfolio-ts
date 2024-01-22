1bdb1c5121a464a25954c3eb17a159cf7dabfec7
Vladislav <labkovich.vl@gmail.com>
2024-01-23 00:56:36 +0300

    feat: add hook UseEffect

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SectionTitle} from "../../../components/SectionTitle";
 import styled from "styled-components";
 import {Container} from "../../../components/Container";
@@ -11,7 +11,6 @@ import beatsLerningImg from "../../../assets/images/beats lerning.png"
 import {Theme} from "../../../styles/Theme";
 import {Button} from "../../../components/Button";
 
-
 const workProjects = [
     {
         src: socialImg,
@@ -23,50 +22,89 @@ const workProjects = [
     {
         src: sendPayImg,
         title: 'SendPay Platform',
-        tags: ['JavaScript', 'Redux'],
+        tags: ['React', 'Redux', 'MongoDB'],
         text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
 
     },
     {
         src: ecommerceImg,
         title: 'E-commerce',
-        tags: ['JavaScript', 'Redux', 'Redis'],
+        tags: ['React', 'Redux', 'Redis'],
         text: 'Mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
 
     },
     {
         src: beatsLerningImg,
         title: 'Beats Lerning',
-        tags: ['JavaScript', 'TypeScript', 'Redux', 'MongoDB'],
+        tags: ['React', 'TypeScript', 'Redux', 'MongoDB'],
         text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
 
     },
 ];
 
+
 export const Projects: React.FC = () => {
+
+    const [showAll, setShowAll] = useState(false);
+    const defaultShow = 2;
+    const showAllByDefault = workProjects.length <= defaultShow;
+
+    const projectsToShow = (showAll || showAllByDefault) ? workProjects : workProjects.slice(0, defaultShow);
+
     return (
         <StyledProjectsContent id="projects">
             <Container>
                 <SectionTitle>Projects</SectionTitle>
+                {/*<FlexWrapper justify={'center'} wrap={'wrap'}>*/}
                 <StyledContent>
-                    {workProjects.map((w) => {
+                    {projectsToShow.map((w) => {
                         return <Project image={w.src}
                                         title={w.title}
                                         tags={w.tags}
                                         text={w.text}
                         />
                     })}
-
-                    <Button width={'305px'} height={'56px'} padding={'12px 20px 13px 20px'} marginTop={'20px'}
-                            btnType={'secondary'} as={'a'} type={'button'}>SEE ALL PROJECTS</Button>
                 </StyledContent>
+
+                    {
+                        showAllByDefault ? null :
+                            <Button onClick={() => setShowAll(val => !val)} width={'305px'} height={'56px'}
+                                    padding={'12px 20px 13px 20px'} marginTop={'20px'}
+                                    btnType={'secondary'} as={'a'}
+                                    type={'button'}>{showAll ? 'HIDE PROJECTS' : 'SEE ALL PROJECTS'}</Button>
+                    }
+
+                {/*</FlexWrapper>*/}
             </Container>
         </StyledProjectsContent>
     );
 };
 
 const StyledProjectsContent = styled.section`
-    margin-bottom: 140px;
+    
+    ${Button} {
+        margin: 36px auto 140px;
+
+    }
+
+
+    @media ${Theme.media.mobile} {
+        margin-bottom: 177px;
+    }
+
+        // ${FlexWrapper} {
+    //     row-gap: 43px;
+    //     column-gap: 27px;
+    //
+        //     @media ${Theme.media.mobile} {
+    //         row-gap: 16px;
+    //         column-gap: 0;
+    //     }
+    // }
+
+    @media ${Theme.media.mobile} {
+
+    }
 `
 
 const StyledContent = styled.div`
@@ -76,16 +114,23 @@ const StyledContent = styled.div`
     row-gap: 43px;
     column-gap: 27px;
 
+    @media ${Theme.media.desktop} {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
     @media ${Theme.media.mobile} {
         row-gap: 16px;
         column-gap: 0;
     }
 
     ${Button} {
-        display: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
         @media ${Theme.media.mobile} {
-            display: flex;
             justify-content: center;
             align-items: center;
         }
