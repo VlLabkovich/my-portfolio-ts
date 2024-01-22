40437b33b5c3b8881db97d58ccbe8f9ac5563de9
Vladislav <labkovich.vl@gmail.com>
2024-01-22 15:55:43 +0300

    refactor: add FlexWrapper

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -5,41 +5,23 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Skill} from "./skill/Skill";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Theme} from "../../../styles/Theme";
+import {skillData} from "./skill/SkillData";
 
 export const Skills: React.FC = () => {
     return (
         <StyledSkillsContent id="skills">
             <Container>
                 <SectionTitle>Tech Skills</SectionTitle>
-                    <StyledSkills>
-                        <Skill iconId={'vscode'} viewBox={'0 0 112 112'}
-                               title={'VS-CODE'}/>
-                        <Skill iconId={'html'}
-                               title={'HTML'}/>
-                        <Skill iconId={'css3'}
-                               title={'CSS3'}/>
-                        <Skill iconId={'js'}
-                               title={'JAVASCRIPT'}/>
-                        <Skill iconId={'ts'} width={'105'}
-                               title={'TYPESCRIPT'}/>
-                        <Skill iconId={'react'}
-                               title={'REACT JS'}/>
-                        <Skill iconId={'sc'}
-                               title={'STYLED COMPONENTS'}/>
-                        <Skill iconId={'bootstrap'}
-                               width={'88'}
-                               height={'120'}
-                               viewBox={'0 0 88 88'}
-                               title={'BOOTSTRAP'}/>
-                        <Skill iconId={'docker'} viewBox={'-10 10 130 100'}
-                               title={'DOCKER'}/>
-                        <Skill iconId={'figma'} viewBox={'0 0 100 100'}
-                               title={'FIGMA'}/>
-                        <Skill iconId={'git'} viewBox={'-6 -4 117 102'}
-                               title={'GIT'}/>
-                        <Skill iconId={'gitHub'} viewBox={'0 -10 86 95'}
-                               title={'GITHUB'}/>
-                    </StyledSkills>
+                <FlexWrapper justify={'space-between'} align={'center'} wrap={'wrap'} >
+                    {skillData.map((s) => {
+                        return <Skill iconId={s.iconId}
+                                      title={s.title}
+                                      width={s.width}
+                                      height={s.height}
+                                      viewBox={s.viewBox}
+                        />
+                    })}
+                </FlexWrapper>
             </Container>
         </StyledSkillsContent>
     );
@@ -47,18 +29,14 @@ export const Skills: React.FC = () => {
 
 const StyledSkillsContent = styled.section`
     margin: 140px 0;
-`
-
-const StyledSkills = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    row-gap: 59px;
-    column-gap: 120px;
     
-    @media ${Theme.media.mobile} {
-        column-gap: 48px;
-        row-gap: 80px;
+    ${FlexWrapper} {
+        row-gap: 59px;
+        column-gap: 120px;
+
+        @media ${Theme.media.mobile} {
+            column-gap: 48px;
+            row-gap: 80px;
+        }
     }
 `
