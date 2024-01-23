d6faa41706bd655e28d81ae3a9ced7bc6158ff6e
Vladislav <labkovich.vl@gmail.com>
2024-01-23 20:46:26 +0300

    refactor: add array objects

---
+++ src/layout/sections/projects/WorkData.tsx
@@ -0,0 +1,35 @@
+import socialImg from "../../../assets/images/social network.webp"
+import sendPayImg from "../../../assets/images/sendpay.png"
+import ecommerceImg from "../../../assets/images/e-commerce.png"
+import beatsLerningImg from "../../../assets/images/beats lerning.png"
+
+export const workProjects = [
+    {
+        src: socialImg,
+        title: 'Social Network',
+        tags: ['JavaScript', 'PostgreSQL', 'React', 'Redux'],
+        text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
+
+    },
+    {
+        src: sendPayImg,
+        title: 'SendPay Platform',
+        tags: ['React', 'Redux', 'MongoDB'],
+        text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
+
+    },
+    {
+        src: ecommerceImg,
+        title: 'E-commerce',
+        tags: ['React', 'Redux', 'Redis'],
+        text: 'Mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
+
+    },
+    {
+        src: beatsLerningImg,
+        title: 'Beats Lerning',
+        tags: ['React', 'TypeScript', 'Redux', 'MongoDB'],
+        text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
+
+    },
+];
