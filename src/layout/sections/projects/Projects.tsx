67690b6160f8ab09abdeb86de89e544d47042ddd
Vladislav <labkovich.vl@gmail.com>
2024-01-23 20:46:39 +0300

    refactor: add import WorkData

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -4,44 +4,9 @@ import styled from "styled-components";
 import {Container} from "../../../components/Container";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Project} from "./project/Project";
-import socialImg from "../../../assets/images/social network.webp"
-import sendPayImg from "../../../assets/images/sendpay.png"
-import ecommerceImg from "../../../assets/images/e-commerce.png"
-import beatsLerningImg from "../../../assets/images/beats lerning.png"
 import {Theme} from "../../../styles/Theme";
 import {Button} from "../../../components/Button";
-
-const workProjects = [
-    {
-        src: socialImg,
-        title: 'Social Network',
-        tags: ['JavaScript', 'PostgreSQL', 'React', 'Redux'],
-        text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
-
-    },
-    {
-        src: sendPayImg,
-        title: 'SendPay Platform',
-        tags: ['React', 'Redux', 'MongoDB'],
-        text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
-
-    },
-    {
-        src: ecommerceImg,
-        title: 'E-commerce',
-        tags: ['React', 'Redux', 'Redis'],
-        text: 'Mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
-
-    },
-    {
-        src: beatsLerningImg,
-        title: 'Beats Lerning',
-        tags: ['React', 'TypeScript', 'Redux', 'MongoDB'],
-        text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.'
-
-    },
-];
-
+import {workProjects} from "./WorkData";
 
 export const Projects: React.FC = () => {
 
