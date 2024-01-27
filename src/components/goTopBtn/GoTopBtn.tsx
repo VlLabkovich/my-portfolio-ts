86bc1cdefba6d22d4166352d3231dd404820faf9
Vladislav <labkovich.vl@gmail.com>
2024-01-27 22:26:22 +0300

    feat: add component

---
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../icon/Icon";
+
+export const GoTopBtn = () => {
+    return (
+        <StyledGoTopBtn>
+            <Icon iconId={'goTop'} />
+        </StyledGoTopBtn>
+    );
+};
+
+const StyledGoTopBtn = styled.button`
+
+`
